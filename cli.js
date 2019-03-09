3774d88a785f0ada7fa91779828c4f165a2dd046
Antoni Kepinski <keppi@o2.pl>
2019-03-09 16:16:15 +0100

    Use meow as a CLI helper, improve code

--- cli.js
+++ cli.js
@@ -3,31 +3,28 @@
 'use strict';
 
 const chalk = require('chalk');
+const meow = require('meow');
 const speedTest = require('speedtest-net');
 const Table = require('cli-table3');
 const ora = require('ora');
 
-const arg = process.argv[2];
 const spinner = ora();
 
 // Help message
-if (arg === '-h' || arg === '--help') {
-	console.log(`
- Usage:
+meow(`
+Usage:
   Just run ${chalk.green.bold('speedo')} to start a speed test!
 
  Powered by ${chalk.cyan('speedtest.net')}
+`);
 
- `);
-	process.exit(0);
-}
-
-// Speed test config
+// Speed test configuration
 const st = speedTest({maxTime: 5250});
+
 // Output in MB/s
 st.on('data', async data => {
-	const download = (data.speeds.download * 0.125).toFixed(2);
-	const upload = (data.speeds.upload * 0.125).toFixed(2);
+	const download = await (data.speeds.download * 0.125).toFixed(2);
+	const upload = await (data.speeds.upload * 0.125).toFixed(2);
 
 	// Table
 	const table = new Table({
@@ -35,24 +32,24 @@ st.on('data', async data => {
 		colWidths: [25, 25]
 	});
 
-	await table.push(
-		[`${chalk.cyan('Download')}`, `${chalk.green(`${download}`)} MB/s`]
-		, [`${chalk.cyan('Upload')}`, `${chalk.green(`${upload}`)} MB/s`]
-		,	[`${chalk.cyan('Latency')}`, `${chalk.green(`${data.server.ping}`)} ms`]
+	table.push(
+		[`${chalk.cyan('Download')}`, `${chalk.green(`${download}`)} MB/s`],
+		[`${chalk.cyan('Upload')}`, `${chalk.green(`${upload}`)} MB/s`],
+		[`${chalk.cyan('Latency')}`, `${chalk.green(`${data.server.ping}`)} ms`]
 	);
 
 	// Print the final report table
-	spinner.succeed(`Done! Here is your speed report:\n`);
+	spinner.succeed('Done! Here is your speed report:\n');
 	console.log(table.toString());
 });
 
 // Download and Upload speed log
-st.on('downloadspeedprogress', speed => {
-	spinner.text = `Testing download speed... ${chalk.green(`${(speed * 0.125).toFixed(2)} MB/s`)}`;
+st.on('downloadspeedprogress', async speed => {
+	spinner.text = `Testing download speed... ${chalk.green(`${(await speed * 0.125).toFixed(2)} MB/s`)}`;
 	spinner.start();
 });
-st.on('uploadspeedprogress', speed => {
-	spinner.text = `Testing upload speed... ${chalk.yellow(`${(speed * 0.125).toFixed(2)} MB/s`)}`;
+st.on('uploadspeedprogress', async speed => {
+	spinner.text = `Testing upload speed... ${chalk.yellow(`${(await speed * 0.125).toFixed(2)} MB/s`)}`;
 	spinner.start();
 });
 
@@ -60,6 +57,6 @@ st.on('uploadspeedprogress', speed => {
 st.on('error', err => {
 	/* istanbul ignore next */
 	if (err.code === 'ENOTFOUND') {
-		console.error(chalk.red.bold(`Unable to connect to the server :(`));
+		console.error(chalk.red.bold('Unable to connect to the server :('));
 	}
 });
